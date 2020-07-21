84f4aa5a7d0092c391c6466d48db8a9d6a3d04e8
MSpilari <mb.spilari@hotmail.com>
2020-07-21 17:49:40 -0300

    Lidando com o CORS

--- src/components/main/searchBox.jsx
+++ src/components/main/searchBox.jsx
@@ -25,9 +25,9 @@ const SearchBox = () => {
     function UserClickButton(){
         let datas = address.split(',').map(data => data.toLowerCase().trim())
         setAddress(datas)
-        fetch(`https://dataservice.accuweather.com/locations/v1/cities/${datas[2]}/${datas[1]}/search?apikey=${APIKEY}&q=${datas[0]}`, 
+        fetch(`https://cors-anywhere.herokuapp.com/dataservice.accuweather.com/locations/v1/cities/${datas[2]}/${datas[1]}/search?apikey=${APIKEY}&q=${datas[0]}`, 
             {
-                mode:'no-cors', 
+                mode:'cors', 
 
             })
             .then(result => result.json())
@@ -37,7 +37,7 @@ const SearchBox = () => {
 
 
     function CityDetails(key){
-        fetch(`https://dataservice.accuweather.com/forecasts/v1/daily/5day/${key}?apikey=${APIKEY}`, {mode:'no-cors'})
+        fetch(`https://cors-anywhere.herokuapp.com/dataservice.accuweather.com/forecasts/v1/daily/5day/${key}?apikey=${APIKEY}`, {mode:'cors'})
             .then(response => response.json())
             .then(data => setInfo(data.DailyForecasts))
     }
